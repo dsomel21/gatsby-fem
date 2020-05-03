dc2f5eeb4d632bf3768beb87742d592e2a0599b4
Dilraj S <dsomel21@gmail.com>
2020-05-03 17:52:51 -0400

    <a> vs Link from Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Link from 'gatsby';
 export default () => (
   <>
     <h1>About Me</h1>
     <p>This is my Medium blog on my website!</p>
+    <a href="/">&larr; Back to Home</a>
+    <Link to="/">&larr; Back to Home</Link>
   </>
 );
